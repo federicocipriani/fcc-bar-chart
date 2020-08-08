69f8fb77934b7f5aef537b79e8b6d560317ceb0f
Federico <64685113+federicocipriani@users.noreply.github.com>
2020-08-08 22:45:31 +0100

    changes-080820

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const w = 800;
+const w = 740;
 const h = 400;
-const padding = 50;
+var margins = { right: 40, left: 80, top: 50, bottom: 50 };
+const padding = 80;
 
-const svg = d3.select('svg').attr('width', w).attr('height', h);
-// .style('border', '1px solid black');
+const svg = d3
+    .select('svg')
+    .attr('width', w + (margins.right + margins.left))
+    .attr('height', h + (margins.top + margins.bottom));
 
 fetch(
     'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
@@ -13,57 +16,85 @@ fetch(
         var binLenght = w / dataset.data.length;
 
         // Arrays containing the x and y axis data
-        var dates = dataset.data.map((item) => new Date(item[0]));
+        const dates = dataset.data.map((item) => item[0]);
+        const datesFormat = dates.map((item) => new Date(item));
         var gdp = dataset.data.map((item) => item[1]);
 
         // Get min and max values
-        var date_min = d3.min(dates);
-        var date_max = d3.max(dates);
+        var date_min = d3.min(datesFormat);
+        var date_max = d3.max(datesFormat);
         var gdp_min = d3.min(gdp);
         var gdp_max = d3.max(gdp);
 
-        console.log(gdp_max);
+        // Adjust max GDP value to get a better graph
+        if (gdp_max % 2000 !== 0) {
+            gdp_max = gdp_max - (gdp_max % 2000) + 2000;
+        }
+
+        // Adjust latest date to get a better graph
+        var date_max_limit = new Date(date_max);
+        date_max_limit.setMonth(date_max_limit.getMonth() + 3);
+
+        // var newdate = date_max.setMonth(date_max.getMonth() + 3);
+        // console.log(newdate);
+
         // Scaling the domain to the dimensions of the canvas
-        const xScale = d3
+        var xScale = d3
             .scaleTime()
-            .domain([date_min, date_max])
-            .range([padding, w - padding]);
-        const yScale = d3
+            .domain([date_min, date_max_limit])
+            .range([margins.left, w + margins.left]);
+
+        var yScale = d3
             .scaleLinear()
             .domain([0, gdp_max])
-            .range([h - padding, padding]);
-
-        console.log(gdp_max);
-        console.log(yScale(gdp_max));
-        // Add axes
-        const xAxis = d3.axisBottom(xScale);
-        const yAxis = d3.axisLeft(yScale);
-
-        svg.append('g')
-            .attr('id', 'x-axis')
-            .attr('transform', 'translate(0,' + (h - padding) + ')')
-            .call(xAxis);
-        svg.append('g')
-            .attr('id', 'y-axis')
-            .attr('transform', 'translate(' + padding + ',0)')
-            .call(yAxis);
+            .range([h + margins.top, margins.top]);
 
         // Render the columns
         d3.select('svg')
             .selectAll('rect')
-            .data(dataset.data)
+            .data(gdp)
             .enter()
             .append('rect')
-            .attr('x', (d, i) => padding + xScale(i * binLenght))
-            .attr('y', (d, i) => h - padding - yScale(d[1]))
+            .attr('x', (d, i) => xScale(datesFormat[i]))
+            .attr('y', (d) => yScale(d))
             .attr('width', binLenght)
-            .attr('height', (d) => yScale(d[1]))
-            .attr('fill', 'teal');
-    });
+            .attr('height', (d) => h + margins.top - yScale(d))
+            .attr('fill', 'teal')
+            .attr('data-date', (d, i) => dates[i])
+            .attr('data-gdp', (d, i) => gdp[i])
+            .attr('class', 'bar')
+            .on('mouseover', (d) => console.log(d));
 
-// d3.json(
-//     'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json',
-//     (dataf) => {
-//         console.log(dataf.data);
-//     }
-// );
+        // Add axes
+        var xAxis = d3.axisBottom(xScale).tickPadding(8);
+        var yAxis = d3.axisLeft(yScale);
+
+        var xAxisGrid = d3.axisTop(xScale).tickFormat('').ticks(0).tickSize(-h);
+        var yAxisGrid = d3.axisLeft(yScale).tickFormat('').tickSize(-w);
+
+        svg.append('g')
+            .attr('id', 'x-axis')
+            .attr('transform', 'translate(0,' + (h + margins.top) + ')')
+            .call(xAxis);
+        svg.append('g')
+            .attr('id', 'x-axis-top')
+            .attr('transform', 'translate(0,' + margins.top + ')')
+            .call(xAxisGrid);
+        svg.append('g')
+            .attr('id', 'y-axis')
+            .attr('transform', 'translate(' + margins.left + ',0)')
+            .call(yAxis);
+        svg.append('g')
+            .attr('id', 'y-axis-grid')
+            .attr('transform', 'translate(' + margins.left + ',0)')
+            .call(yAxisGrid);
+        svg.append('text')
+            .text('Gross Domestic Product (GDP)')
+            .attr('transform', 'rotate(-90)')
+            .attr('x', -(h / 1.5 + margins.top))
+            .attr('y', margins.left / 3);
+        svg.append('text')
+            .text('Years')
+            .attr('x', margins.left + w / 2.1)
+            .attr('y', h + 1.9 * margins.top);
+    });
